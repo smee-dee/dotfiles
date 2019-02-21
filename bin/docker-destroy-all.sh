aaeaf7f78d1cf31d74a33b1e2c2652a5cee8f173
Stefan Schmidt <stefan.schmidt@aroundhome.de>
2019-02-21 10:51:02 +0100

    Add destroy all docker containers script

---
+++ bin/docker-destroy-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker stop $(docker ps -a -q)
+
+docker rm $(docker ps -q -f 'status=exited')
+docker volume rm $(docker volume ls -q -f dangling=true)
+docker rmi $(docker images -f dangling=true -q)
+
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
