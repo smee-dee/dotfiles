c517fd142237075f469d411869ae078bb296335c
Stefan Schmidt <stefan.schmidt@aroundhome.de>
2019-02-21 12:36:42 +0100

    Force destroy

--- bin/docker-destroy-all.sh
+++ bin/docker-destroy-all.sh
@@ -7,4 +7,4 @@ docker volume rm $(docker volume ls -q -f dangling=true)
 docker rmi $(docker images -f dangling=true -q)
 
 docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker rmi $(docker images -qa) --force
